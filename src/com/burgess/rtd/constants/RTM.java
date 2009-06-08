a787b2123ffa0cca2ed0e895c55a07d0aca9737f
Andrew Burgess <abombm1@gmail.com>
2009-06-08 11:17:03 -0400

    Modified the ignore file

--- src/com/burgess/rtd/constants/RTM.java
+++ src/com/burgess/rtd/constants/RTM.java
@@ -1,16 +1,16 @@
 /**
- * RTM.java
- * com.burgess.rtd.constants
- *
- * Created Jun 3, 2009
- *
- * Author: Andrew Burgess
- * Email: abombm1@gmail.com
- * Copyright: 2009
- */
+* RTM.java
+* com.burgess.rtd.constants
+*
+* Created Jun 3, 2009
+*
+* Author: Andrew Burgess
+* Email: abombm1@gmail.com
+* Copyright: 2009
+*/
 package com.burgess.rtd.constants;
-
+ 
 public class RTM {
-	public static final String API_KEY = "c54b775ba175d358b9adf622a1fd383f";
-	public static final String SHARED_SECRET = "3af29ded6429d6bc";
+  public static final String API_KEY = "c54b775ba175d358b9adf622a1fd383f";
+  public static final String SHARED_SECRET = "3af29ded6429d6bc";
 }
