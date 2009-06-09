a3becd6362c3b0bd564274316a2521a751c92029
Andrew Burgess <abombm1@gmail.com>
2009-06-09 15:46:26 -0400

    Cleaned up imports

--- src/com/burgess/rtd/ConfigureActivity.java
+++ src/com/burgess/rtd/ConfigureActivity.java
@@ -18,7 +18,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
